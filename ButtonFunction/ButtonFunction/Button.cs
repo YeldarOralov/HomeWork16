550820431e360caab94ded092e4b0c04e4036ab1
YeldarOralov <YeldarOralov@users.noreply.github.com>
2019-03-27 11:40:13 +0600

    Добавление файлов проекта

---
+++ ButtonFunction/ButtonFunction/Button.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ButtonFunction
+{
+    public delegate void Click(string text);
+    public class Button
+    {
+        public event Click Click;
+        public void OnClick()
+        {
+            Click("Click");
+        }
+    }
+}
---
+++ ButtonFunction/ButtonFunction/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ButtonFunction
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {            
+
+            var button = new Button();
+            button.Click += ShowText;
+
+            while (true)
+            {
+                var consoleKey = Console.ReadKey().Key;
+                if(consoleKey == ConsoleKey.Spacebar)
+                {
+                    button.OnClick();
+                }
+            }
+        }
+        public static void ShowText(string text)
+        {
+            Console.WriteLine(text);
+        }
+    }
+}
